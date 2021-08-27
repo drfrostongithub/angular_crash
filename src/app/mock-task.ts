5675976ea7c034e9c474471586ed83fb091a77df
drfrostongithub <eternal.putra@gmail.com>
2021-08-27 14:01:06 +0700

    second commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Vacinnation',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Dota Meeting',
+    day: 'May 6th at 2:00pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Game Shopping',
+    day: 'May 7th at 2:30pm',
+    reminder: false,
+  },
+];
